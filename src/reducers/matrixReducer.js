5973cc07bc70353cdaec3a6efa6a1ca3cfc2f339
λ • genesis <rwxgenesis@gmail.com>
2022-01-02 20:00:33 +0300

    Update solution

--- src/reducers/matrixReducer.js
+++ src/reducers/matrixReducer.js
@@ -1,5 +1,7 @@
 import fp from "lodash/fp";
 
+fp.map = fp.map.convert({ cap: false });
+
 export default function matrixReducer(state, action) {
   switch (action.type) {
     case "setMatrix":
@@ -12,7 +14,7 @@ export default function matrixReducer(state, action) {
             fp.map(
               (n, j) =>
                 i === action.payload.i && j === action.payload.j
-                  ? fp.parseInt(action.payload.value)
+                  ? fp.parseInt(action.payload.value, 10)
                   : n,
               row
             ),
